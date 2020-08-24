831f5e281aeeab51ae2102c8f6e0dcabc93c4223
Nestor <ncervan87@gmail.com>
2020-08-24 12:31:30 -0700

    add react router, protected routes, mock api call

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, Switch, Route } from 'react-router-dom'
 
 import Dashboard from './Dashboard'
 import PrivateRoute from './PrivateRoute'
 import Navbar from './Navbar'
 import SavedPosts from './SavedPosts'
+import axios from 'axios'
 
 
 function App() {
 
   const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    axios.get('https://jsonblob.com/api/jsonBlob/9af77dc1-e634-11ea-b735-83f87d192444')
+      .then(res => {
+        console.log(res.data.payload)
+        localStorage.setItem('token', res.data.payload)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
 
   return (
     <div className="App">
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import axios from 'axios'
 
 function Dashboard(props) {
+
+    //using useEffect at the time being until we set up a data management
+
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        axios.get(`https://jsonblob.com/api/jsonBlob/824df1e3-e636-11ea-b735-85daf3898380`)
+            .then(res => {
+                setData(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }, [])
+
+
+    const cards = data.map(item => (
+        <div>
+            <h3>{item.username}</h3>
+            <p>{item.troll}</p>
+            <p>{item.comment}</p>
+        </div>
+    ))
+
     return (
         <div>
-            Dashboard
+            {cards}
         </div>
     )
 }
