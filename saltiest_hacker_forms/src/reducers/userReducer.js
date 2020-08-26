017a458e00d69a415004bef08faa0a6a55aaac9b
Nestor <ncervan87@gmail.com>
2020-08-25 22:49:50 -0700

    refactor SavedPosts.js and action creator for component.  Other minor refactoring

--- saltiest_hacker_forms/src/reducers/userReducer.js
+++ saltiest_hacker_forms/src/reducers/userReducer.js
@@ -5,6 +5,11 @@ const initialState = {
 
 export const userReducer = (state = initialState, action) => {
     switch (action.type) {
+        case 'FETCHED_SAVED':
+            return {
+                ...state,
+                savedPosts: action.payload
+            }
         default:
             return state
     }
